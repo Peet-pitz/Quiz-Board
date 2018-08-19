cdd1ee773d75d25da62ea627f2c6de39bef7f1ca
Peter Njoroge <njoroge.pitz@gmail.com>
2018-08-19 07:34:29 +0300

    add question 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $("form#quiz").submit(function(event){
 var q1= $("input:radio[name=q1]:checked").val();
 var q2= $("input:radio[name=q2]:checked").val();
 var q3= $("input:radio[name=q3]:checked").val();
-var result=parseInt(q1)+parseInt(q2)+parseInt(q3);
+var q4= $("input:radio[name=q4]:checked").val();
+var q5= $("input:radio[name=q5]:checked").val();
+var result=parseInt(q1)+parseInt(q2)+parseInt(q3)+parseInt(q4)+parseInt(q5);
 $("#result").text("You got "+result);
 
 $(".quiz").hide();
